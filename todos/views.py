c7b1ca707eb51eecb4ba5625b66547f1eb76c77d
moong3871 <moong3871@gmail.com>
2020-12-05 22:42:52 +0900

    [문정] feat:todos Create,Read

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .models import Todo
+from .serializers import TodoSerializer
 # Create your views here.
+
+@api_view(['GET','POST'])
+def todo_list_create(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
