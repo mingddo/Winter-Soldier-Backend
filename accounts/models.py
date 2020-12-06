1eb1cfaa1d24917dc406eb5cddf94ec0243a9dff
moong3871 <moong3871@gmail.com>
2020-12-06 14:54:17 +0900

    [문정]feat: follower,following

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='followings')
