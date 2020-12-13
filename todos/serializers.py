85cea17114579b3d53845f7364219adf8d88dbb8
moong3871 <moong3871@gmail.com>
2020-12-13 14:40:16 +0900

    [문정] fix: decoration 추가 및 views.py 수정

--- todos/serializers.py
+++ todos/serializers.py
@@ -6,3 +6,4 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
+        read_only_fields = ('user',)
--- todos/views.py
+++ todos/views.py
@@ -4,22 +4,30 @@ from rest_framework.response import Response
 
 from .models import Todo
 from .serializers import TodoSerializer
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 # Create your views here.
 
 @api_view(['GET','POST'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def todo_list_create(request):
-    if request.user.is_authenticated:
-        if request.method == 'GET':
-            todos = Todo.objects.all()
-            serializer = TodoSerializer(todos, many=True)
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
             return Response(serializer.data)
-        else:
-            serializer = TodoSerializer(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
+
 
 @api_view(['PUT', 'DELETE'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def todo_update_delete(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk)
     if request.user == todo.user:
