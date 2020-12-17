8bc0b4fe85799713bb01b1d4cd94d9ae26fde8d8
moong3871 <moong3871@gmail.com>
2020-12-17 18:15:55 +0900

    [문정] related_name 추가

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Todo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="my_todo")
     title = models.CharField(max_length=100)
     schedule_year = models.IntegerField
     schedule_month = models.IntegerField()
@@ -14,4 +14,7 @@ class Todo(models.Model):
     alarm_date = models.IntegerField()
     alarm_hour = models.IntegerField()
     alarm_min = models.IntegerField()
+
+    def __str__(self):
+        return self.title
     
