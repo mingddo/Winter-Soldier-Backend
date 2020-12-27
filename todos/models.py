5cf0689e1fbccd757bed6d0707a4f277bf5157a2
moong3871 <moong3871@gmail.com>
2020-12-27 20:37:28 +0900

    [문정]fix: todo 모델과 serializer 추가

--- todos/models.py
+++ todos/models.py
@@ -14,6 +14,7 @@ class Todo(models.Model):
     alarm_date = models.IntegerField()
     alarm_hour = models.IntegerField()
     alarm_min = models.IntegerField()
+    completed = models.CharField(max_length=10, default="no")
 
     def __str__(self):
         return self.title
