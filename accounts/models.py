b14b9ffe65744e3042f1fff7daa06ad8fbe039d4
zngsm <68373473+zngsm@users.noreply.github.com>
2021-01-03 23:06:30 +0900

    [수민] feat: group 모델링 + 회원 삭제기능 추가 / group todo는 XXX

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import sys ; sys.path.append('/dir/of/todos')
+from todos.models import Todo
 
 class User(AbstractUser):
     followers = models.ManyToManyField('self', symmetrical=False, related_name='followings')
+
+
+class Group(models.Model):
+    user = models.ManyToManyField(User, related_name='group')
+    inviting = models.ManyToManyField(User, related_name='invited')
+    name = models.CharField(max_length=20)
+    master = models.CharField(max_length=50)
+    todo = models.ManyToManyField(Todo, related_name="group_Todo")
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,14 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('signup/', views.signup),
     path('api_token_auth/', obtain_jwt_token),
+    path('delete/<username>/', views.userdelete),
     path('userlist/', views.get_userlist),
     path('userlist/<keyword>/', views.find_userlist),
     path('profile/<username>/', views.get_profile),
     path('follow/<username>/', views.follow),
+    path('group/', views.group),
+    path('group/<int:group_pk>/', views.group_detail),
+    path('group/<int:group_pk>/invite/<username>/', views.group_invite),
+    path('group/<int:group_pk>/member/<username>/', views.group_member),
+    path('group/<int:group_pk>/master/<username>/', views.group_master),
 ]
