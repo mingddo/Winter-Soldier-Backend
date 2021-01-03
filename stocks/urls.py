fef1cf23e19df8bdeef4ac9c7c43abc33831bb4a
jes5918 <68365206+jes5918@users.noreply.github.com>
2021-01-03 14:07:06 +0900

    [의수] feat: 주식자동완성

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'stocks'
+app_name = "stocks"
 
 urlpatterns = [
-    path('<str:companyname>/', views.stocks, name="stocks"),
+    path("<str:companycode>/<int:period>/", views.stocks, name="stocks"),
 ]
