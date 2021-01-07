02ab65902582112f6681e2ff744afea839455891
전의수 <jes5918@koreatech.ac.kr>
2021-01-07 23:20:44 +0900

    [의수] fix: stock 최종수정

--- stocks/views.py
+++ stocks/views.py
@@ -17,27 +17,28 @@ def stocks(request, companycode, period):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
     }
-    # for page in range(1, period):
-    url = "http://finance.naver.com/item/sise_day.nhn?code={companycode}".format(
-        companycode=companycode
-    )
-    url = "{url}&page={page}".format(url=url, page=1)
 
-    response = requests.get(url, headers=headers)
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
-    for t in soup.select(".tah.p10"):
-        date.append(t.text[5:])
+    for page in range(1, period):
+        url = "http://finance.naver.com/item/sise_day.nhn?code={companycode}".format(
+            companycode=companycode
+        )
+        url = "{url}&page={page}".format(url=url, page=page)
 
-    temp = []
-    for tt in soup.select(".tah.p11"):
-        # print(tt.text)
-        temp.append(tt.text)
-    print(temp)
+        response = requests.get(url, headers=headers)
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+        for t in soup.select(".tah.p10"):
+            date.append(t.text[5:])
 
-    for i in range(10):
-        price.append(temp[6 * i])
-        volume.append(temp[6 * i + 5])
+        temp = []
+        for tt in soup.select(".tah.p11"):
+            # print(tt.text)
+            temp.append(tt.text)
+        # print(temp)
+
+        for i in range(10):
+            price.append(int("".join(temp[6 * i].split(","))))
+            volume.append(int("".join(temp[6 * i + 5].split(","))))
 
     context = {
         "date": date,
